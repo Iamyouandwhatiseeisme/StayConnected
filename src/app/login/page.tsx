4fc6c65eda2da25bd7691b44c0d46ed87fd37949
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-11-29 13:49:30 +0400

    fix(cleanup): cleanup extra code that was meant for signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <div className="h-screen w-full bg-gradient-to-r from-blue-500  via-purple-500 to-pink-500 animate-gradient flex flex-col items-center justify-center ">
       <div className="h-3/4 w-150 border-2 border-white rounded-2xl bg-gray-200 flex flex-col items-center justify-center">
-        <div className="bg-white w-120 h-80 border border-gray-500 rounded-2xl flex flex-col items-center justify-end p-5">
+        <div className="bg-white w-120 h-80 border border-gray-500 rounded-2xl flex flex-col items-center justify-start p-5">
           <div className="border  border-gray-400  bg-white rounded-2xl w-40 h-10 flex flex-row text-sm overflow-hidden items-center justify-center">
             <img
               className=" object-fill h-10 rounded-2xl"
@@ -26,14 +26,9 @@ export default function Login() {
             placeholder="Password"
           />
 
-          <Input
-            className="p-2 m-2 w-80"
-            type="password"
-            placeholder="Confirm Password"
-          />
           <div className="flex flex-row gap-10">
-            <Button className="bg-gray-400">Login</Button>
-            <Button>Register</Button>
+            <Button>Login</Button>
+            <Button className="bg-gray-400">Register</Button>
           </div>
         </div>
       </div>
